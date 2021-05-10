fe554113785da1b212954b43517a47585aabe355
John Phillips <coldlombax@gmail.com>
2021-05-10 10:14:55 +0100

    Added waiting, error and input popup information.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import Background from "./components/Background/Background";
 import classes from "./App.module.scss";
 
 function App() {
-  const [binary, setBinary] = useState(0);
-  const [decimal, setDecimal] = useState(0);
+  const [binary, setBinary] = useState("");
+  // const [decimal, setDecimal] = useState(0);
 
   // CONVERTS BINARY INTO DECIMAL AND SETS STATE
-  const binaryHandler = (event) => {
-    setBinary(event.target.value);
-    const digit = parseInt(event.target.value, 2);
-    setDecimal(digit);
-  };
+  // const binaryHandler = (event) => {
+  //   setBinary(event.target.value);
+  //   const digit = parseInt(event.target.value, 2);
+  //   setDecimal(digit);
+  // };
 
   // CONVERTS DECIMAL INTO BINARY AND SETS STATE
-  const decimalHandler = (event) => {
-    setDecimal(event.target.value);
-    const number = parseInt(event.target.value);
-    const result = number.toString(2);
-    setBinary(result);
+  // const decimalHandler = (event) => {
+  //   setDecimal(event.target.value);
+  //   const number = parseInt(event.target.value);
+  //   const result = number.toString(2);
+  //   setBinary(result);
+  // };
+
+  const [error, setError] = useState(false);
+
+  const binaryHandler = (event) => {
+    const stringNumber = event.target.value;
+    const pattern = new RegExp("[2-9]");
+    if (pattern.test(stringNumber)) {
+      setError(true);
+      console.log("has a number greater than 1");
+    } else {
+      setError(false);
+    }
+    setBinary(event.target.value);
   };
 
   return (
@@ -32,8 +46,22 @@ function App() {
           </p>
         </header>
         <section className={classes.main}>
-          <input className={classes.input}></input>
-          <p className={classes.value}>Waiting for Input...</p>
+          <input
+            type="number"
+            pattern="1(0∗1)∗ "
+            value={binary}
+            onChange={binaryHandler}
+            className={classes.input}
+          ></input>
+          {error ? (
+            <p className={classes.error}>Invalid Binary Number</p>
+          ) : (
+            <p className={classes.value}>{binary}</p>
+          )}
+
+          {binary === "" && (
+            <p className={classes.waiting}>Waiting for Input...</p>
+          )}
         </section>
         <footer>
           <p className={classes.stamp}>
